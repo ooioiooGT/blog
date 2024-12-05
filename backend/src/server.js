f356006a7affbab0d558ee745572574c1eaf0b5f
ooioiooGT <97373570+ooioiooGT@users.noreply.github.com>
2024-12-05 01:42:43 -0700

    learning how to upvote for the article and leave a comment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,46 @@
 import express from 'express';
 
+let articlesInfo = [{
+  name: 'learn-react',
+  upvotes: 0,
+  comments: [],
+},{
+  name:'learn-node',
+  upvotes: 0, 
+  comments: [],
+},{
+  name: 'mongodb',
+  upvotes: 0, 
+  comments: [],
+}]
+
 const app = express();
 app.use(express.json());
 
-
+//this apit count how many times that user click
 app.put('/api/articles/:name/upvote', (req,res) => {
+  const {name} = req.params;
+  const article = articlesInfo.find(a => a.name ===name); 
+  if ( article){
+    article.upvotes += 1; 
+    res.send(`the ${name} article now has ${article.upvotes} upvote`);
+  }else{
+    res.send('That article dos not exist')
+  }
+});
 
+app.post('/api/articles/:name/comments', (req,res) => {
+  const {name} = req.params;
+  const {postby, comment} = req.body;
+  const article = articlesInfo.find(a => a.name === name);
+  if(article){
+    article.comments.push({postby, comment});
+    res.send(article.comments);
+  }else{
+    res.send('That article dos not exist')
+  }
+  
 })
-
-
 
 
 
